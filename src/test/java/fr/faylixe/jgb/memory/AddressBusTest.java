884ad5eabff242db51aa7ef0b240a740ad48532a
Faylixe <felix.voituret@gmail.com>
2018-03-19 13:54:27 +0100

    Fix address bus unit test

--- src/test/java/fr/faylixe/jgb/memory/AddressBusTest.java
+++ src/test/java/fr/faylixe/jgb/memory/AddressBusTest.java
@@ -1,57 +1,47 @@
 package fr.faylixe.jgb.memory;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-
-import fr.faylixe.jgb.MockitoExtension;
 
 /**
  * 
  * @author fv
  */
-@ExtendWith(MockitoExtension.class)
 public final class AddressBusTest implements IMemoryStreamTest {
 
 	/** Target test bus. **/
 	private AddressBus addressBus;
 
 	/** **/
-	@Mock
-	private IMemoryBank mainBank;
+	private IMemoryBank bank1;
 
 	/** **/
 	@BeforeEach
 	public void setup() {
 		addressBus = new AddressBus();
-		when(mainBank.getSize()).thenReturn(TEST_SIZE);
-		when(mainBank.getOffset()).thenReturn(TEST_OFFSET);
-		//when(mainBank.readByte(any(Integer.class)))
+		bank1 = IMemoryBankTest.createMemoryBankMock();
+		addressBus.connect(bank1);
 	}
 
 	/** {@inheritDoc} **/
 	@Override
 	public IMemoryStream getTestMemoryStream() {
-		// TODO : Configure address bus with plugged bank here.
 		return addressBus;
 	}
 
 	/** **/
 	@Test
-	public void testConnect() {
+	public void testIllegalconnect() {
 		assertThrows(IllegalArgumentException.class, () -> addressBus.connect(null));
-		//addressBus.connect();
+		// TODO : Check for conflicting space.
 	}
 
 	/** **/
 	@Test
-	public void testDisconnect() {
+	public void testIllegalDisconnect() {
 		assertThrows(IllegalArgumentException.class, () -> addressBus.disconnect(null));		
-		// TODO : Test out of range value.
 	}
 
 }
