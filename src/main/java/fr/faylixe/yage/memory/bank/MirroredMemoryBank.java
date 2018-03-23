bc8d7856abd4a1f9a38b0f3ba1a22130399b1535
fv <felix.voituret@gmail.com>
2018-03-23 15:58:24 +0100

    Add mirrored memory bank

---
+++ src/main/java/fr/faylixe/yage/memory/bank/MirroredMemoryBank.java
@@ -0,0 +1,17 @@
+package fr.faylixe.yage.memory.bank;
+
+import fr.faylixe.yage.memory.IMemoryBank;
+
+/**
+ * 
+ * @author fv
+ */
+public final class MirroredMemoryBank {
+
+	/** **/
+	private final IMemoryBank [] mirroredBanks;
+
+	public MirroredMemoryBank() {
+		this.mirroredBanks = null;
+	}
+}
