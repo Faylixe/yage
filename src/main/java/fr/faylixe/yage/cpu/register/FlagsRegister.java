d10c5e15de12419a33e60bf099f347f4cc2e5364
Faylixe <felix.voituret@gmail.com>
2018-03-26 08:55:57 +0200

    Implements flags setter

--- src/main/java/fr/faylixe/yage/cpu/register/FlagsRegister.java
+++ src/main/java/fr/faylixe/yage/cpu/register/FlagsRegister.java
@@ -1,11 +1,57 @@
 package fr.faylixe.yage.cpu.register;
 
 /**
- * TODO : Consider using symmetric flag setter.
+ * Specific byte register that provides sugar methods
+ * for bit flag access. Such flags are :
+ * 
+ * - Zero flag (Z) located at bit 7.
+ * - Subtraction flag (N) located at bit 6.
+ * - Half carry flag (H) located at bit 5.
+ * - Carry flag (C) located at bit 4.
  * 
  * @author fv
  */
 public final class FlagsRegister extends ByteRegister {
+
+	/** Sets the zero flag (Z) to 1. **/
+	public void setZero() {
+		set((byte) (get() | (1 << 7)));
+	}
+
+	/** Resets the zero flag (Z) to 0. **/
+	public void resetZero() {
+		set((byte) (get() & ~(1 << 7)));
+	}
+
+	/** Sets the subtraction flag (N) to 1. **/
+	public void setSubtraction() {
+		set((byte) (get() | (1 << 6)));
+	}
+
+	/** Resets the subtraction flag (N) to 0. **/
+	public void resetSubtraction() {
+		set((byte) (get() & ~(1 << 6)));
+	}
+
+	/** Sets the half carry flag (H) to 1. **/
+	public void setHalfCarry() {
+		set((byte) (get() | (1 << 5)));
+	}
+
+	/** Resets the half carry flag (H) to 0. **/
+	public void resetHalfCarry() {
+		set((byte) (get() & ~(1 << 5)));
+	}
+
+	/** Sets the carry flag (C) to 1. **/
+	public void setCarry() {
+		set((byte) (get() | (1 << 4)));
+	}
+
+	/** Resets the carry flag (C) to 0. **/
+	public void resetCarry() {
+		set((byte) (get() & ~(1 << 4)));
+	}
 
 	/**
 	 * Indicates if zero flag (Z) is settled.
@@ -17,11 +63,11 @@ public final class FlagsRegister extends ByteRegister {
 	}
 
 	/**
-	 * Indicates if substraction flag (N) is settled.
+	 * Indicates if subtraction flag (N) is settled.
 	 * 
-	 * @return <tt>true</tt> if substraction flag is settled, <tt>false</tt> otherwise.
+	 * @return <tt>true</tt> if subtraction flag is settled, <tt>false</tt> otherwise.
 	 */
-	public boolean isSubstraction() {
+	public boolean isSubtraction() {
 		return ((get() >> 6) & 1) == 1;
 	}
 
