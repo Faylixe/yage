5b27ce6bab6715eada6bd11aba1d778343ea4b94
Faylixe <felix.voituret@gmail.com>
2018-04-01 15:37:17 +0200

    Update register package-info

--- src/main/java/fr/faylixe/yage/cpu/register/package-info.java
+++ src/main/java/fr/faylixe/yage/cpu/register/package-info.java
@@ -1,8 +1,7 @@
 /**
+ * Provides several implementation of a CPU register,
+ * all aimed to be accessed through IRegisterProvider interface.
  * 
- */
-/**
  * @author fv
- *
  */
 package fr.faylixe.yage.cpu.register;
